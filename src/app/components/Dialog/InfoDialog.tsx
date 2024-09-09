e1f70d1ba4e35b2c3f72d266e3e0e73d13538d8e
vibhor <elementswaer@gmail.com>
2024-09-09 01:23:08 -0400

    fixed apostrophe linting errors

--- src/app/components/Dialog/InfoDialog.tsx
+++ src/app/components/Dialog/InfoDialog.tsx
@@ -28,11 +28,11 @@ export const InfoDialog = ({ onDialogClick }: DialogInterface)=> {
       <StyledInfoDialog>
         <Title>inspo</Title>
         <Content>
-        <p>hi y’all!</p>
+        <p>hi y&#39;all!</p>
         <p>this past 2024 winter, i was fortunate enough to embark on an exchange term in taiwan for 4 months.</p>
-        <p>during my time there, i experienced some of the most surreal sights ⛰️, met some really cool people 🧍, and created core memories i’ll look back to many years down the road. 
+        <p>during my time there, i experienced some of the most surreal sights ⛰️, met some really cool people 🧍, and created core memories i&#39;ll look back to many years down the road. 
         coming back from exchange, i reminisced over the vibrant culture of taiwan and wanted a way to feel connected to its culture and people.</p>
-        <p>with inspiration from <a href="https://imissmycafe.com" target="_blank" style={linkStyle}><b>imissmycafe ☕</b></a>, and my girlfriend k’s idea of creating something similar but for taiwan, 
+        <p>with inspiration from <a href="https://imissmycafe.com" target="_blank" style={linkStyle}><b>imissmycafe ☕</b></a>, and my girlfriend k&#39;s idea of creating something similar but for taiwan, 
         this personal artifact was brought to life.
         although a few sounds are from my own footage, i also want to give credit to <a href="https://taiwanquest.com" target="_blank" style={linkStyle}><b>taiwan quest 🇹🇼</b></a> and <a href="https://fh-potsdam.github.io/Sounds-of-Taipei/" target="_blank" style={linkStyle}><b>postdam's sounds of taipei</b></a> for their catalog of sounds to use.</p>
         <p>come and immerse yourselves in some of the most prominent sounds taiwan has to offer :) </p>
